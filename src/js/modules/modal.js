c021c463de55a85ea6783ceaf78ea9c3211e0e39
Andrew <whitedrew538@gmail.com>
2022-01-29 00:27:03 +0500

    popup

---
+++ src/js/modules/modal.js
@@ -0,0 +1,56 @@
+const modal = () => {
+
+    function modalWindow(triggerSelector, modalSelector) {
+        const trigger = document.querySelectorAll(triggerSelector);
+        const modalWindow = document.querySelector(modalSelector);
+        const windows = document.querySelectorAll('[data-modal]');
+
+        trigger.forEach(el => {
+            el.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+
+                windows.forEach(window => window.style.display = 'none');
+
+                modalWindow.style.display = 'block';
+                document.body.style.overflow = 'hidden';
+                ///clearInterval(() => showModalByTime);
+            });
+        })
+
+        modalWindow.addEventListener('click', (e) => {
+            if (e.target.className === modalSelector.slice(1) || e.target.className === 'popup-close') {
+                modalWindow.style.display = 'none';
+                document.body.style.overflow = '';
+                document.body.style.marginRight = '0px';
+            }
+        })
+
+    }
+
+    modalWindow('.button-consultation', '.popup-consultation');
+    modalWindow('.button-design', '.popup-design');
+
+    function showModalByTime(selector) {
+        setTimeout(function () {
+            let display;
+
+            document.querySelectorAll('[data-modal]').forEach(el => {
+                if (getComputedStyle(el).display !== 'none') {
+                    display = 'block';
+                }
+            });
+
+            if (!display) {
+                document.querySelector(selector).style.display = 'block';
+                document.body.style.overflow = 'hidden';
+            }
+        }, 5000)
+    }
+
+    showModalByTime('.popup-consultation');
+
+}
+
+export default modal;
